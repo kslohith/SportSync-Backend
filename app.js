4a1450a3de329f9fb12f45119c3a0d4220dc3dee
Lohith K S <lohithks@Lohiths-MacBook-Pro-2.local>
2023-10-16 01:08:40 -0400

    Enabling cors for api

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+//enable cors for everyone
+app.use(cors());
 
 app.set('view engine', null);
 
